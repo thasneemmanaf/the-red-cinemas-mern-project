25d5c6603884378bd16518610eee73ebff7ad4f3
thasneemmanaf <123thasneemsalim@gmail.com>
2020-10-31 20:17:26 +0100

    Add carousal affect for banner image

--- client/src/components/Banner/index.js
+++ client/src/components/Banner/index.js
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 import axios from '../../axios';
 import './Banner.css';
 
 export default function Banner() {
-  const [movie, setMovie] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       const response = await axios.get('/movie');
-      setMovie(
-        response.data.movies[
-          Math.floor(Math.random() * response.data.movies.length)
-        ]
-      );
+      setMovies(response.data.movies);
       return response;
     }
     fetchData();
@@ -22,31 +22,62 @@ export default function Banner() {
   function truncate(str, n) {
     return str.length > n ? `${str.substr(0, n - 1)}...` : str;
   }
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundSize: 'cover',
-        backgroundImage: `url(
-        ${movie.bannerImage}
-        )`,
-        backgroundPosition: 'center center'
-      }}>
-      <div className="banner_contents ">
-        <h1 className="banner_title">{movie.title}</h1>
-        <div className="banner_buttons">
-          <button className="banner_button" type="button">
-            INFO
-          </button>
-          <button className="banner_button" type="button">
-            BOOK NOW
-          </button>
-        </div>
-        <h1 className="banner_description">
-          {movie.description && truncate(movie.description.en, 150)}
-        </h1>
+
+  console.log(movies);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 200000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: 'ease-in-out',
+    initialSlide: 4,
+    pauseOnDotsHover: true,
+    appendDots: (dots) => (
+      <div
+        style={{
+          backgroundColor: '#fff',
+          borderRadius: '10px',
+          padding: '10px',
+          maxWidth: '50%'
+        }}>
+        <ul style={{ margin: '0px', backgroundColor: '#fff' }}>{dots}</ul>
       </div>
-      <div className="banner_fadeBottom" />
-    </header>
+    )
+  };
+  return (
+    <>
+      <Slider {...settings}>
+        {movies.map((movie) => {
+          return (
+            <div className="banner" key={movie._id}>
+              <img
+                src={movie.bannerImage}
+                alt={movie.name}
+                style={{ width: '100%', maxHeight: '100%' }}
+              />
+              <div className="banner_contents ">
+                <h1 className="banner_title">{movie.title}</h1>
+                <div className="banner_buttons">
+                  <button className="banner_button" type="button">
+                    INFO
+                  </button>
+                  <button className="banner_button" type="button">
+                    BOOK NOW
+                  </button>
+                </div>
+                <h1 className="banner_description">
+                  {movie.description && truncate(movie.description.en, 150)}
+                </h1>
+              </div>
+              <div className="banner_fadeBottom" />
+            </div>
+          );
+        })}
+      </Slider>
+    </>
   );
 }
