42e47d62649b18019bb9e3c84369acbc49c2955d
thasneemmanaf <123thasneemsalim@gmail.com>
2020-11-04 01:06:31 +0100

    Add SignIn and SignUp buttons

---
+++ client/src/components/AccountMenu/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './AccountMenu.module.css';
+
+function AccountMenu() {
+  return (
+    <div className={classes.nav}>
+      <ul>
+        <li>
+          <a href="#bjfghhf">Sign In</a>
+        </li>
+        <li>
+          <a href="#ffg">Sign Up</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default AccountMenu;
--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import './Navbar.css';
+import AccountMenu from '../AccountMenu';
 
 function Navbar() {
   const [show, handleshow] = useState(false);
+  const [accountShow, setAccountShow] = useState(false);
 
   const handleScroll = useCallback(() => {
     if (window.scrollY > 100) {
@@ -17,6 +19,15 @@ function Navbar() {
     };
   }, []);
 
+  // To dislay signin and signup
+  const onclickAccount = () => {
+    if (!accountShow) {
+      setAccountShow(true);
+    } else {
+      setAccountShow(false);
+    }
+  };
+
   return (
     <div className={`nav ${show && 'nav_black'}`}>
       <img
@@ -24,11 +35,17 @@ function Navbar() {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/250px-Netflix_2015_logo.svg.png"
         alt="Netflix Logo"
       />
-      <img
-        className="nav_avatar"
-        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
-        alt="Netflix Logo"
-      />
+      <button
+        type="button"
+        style={{ background: 'none', border: 'none' }}
+        onClick={onclickAccount}>
+        <img
+          className="nav_avatar"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+          alt="Netflix Logo"
+        />
+      </button>
+      {accountShow && <AccountMenu />}
     </div>
   );
 }
