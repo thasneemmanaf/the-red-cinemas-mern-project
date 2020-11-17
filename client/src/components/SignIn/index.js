1d3768863ed56b4b306bb5596e7afc46b7678630
thasneemmanaf <123thasneemsalim@gmail.com>
2020-11-17 18:16:40 +0100

    Add password confirm field to Signup form and add styling modifications to both signin and signup forms

--- client/src/components/SignIn/index.js
+++ client/src/components/SignIn/index.js
@@ -4,45 +4,40 @@ import classes from './SignIn.module.css';
 function SignIn() {
   return (
     <div className={classes.form}>
-      <form className={classes.login_form} action="./" method="POST">
-        <div className={classes.login_form__content}>
-          <div className={classes.login_form__header}>
-            SignIn to your account
+      <form className={classes.signin_form} action="./" method="POST">
+        <div className={classes.signin_form__content}>
+          <div className={classes.signin_form__header}>
+            Signin to your account
           </div>
           <input
-            className={classes.login_form__input}
+            className={classes.signin_form__input}
             type="text"
             name="name"
             placeholder="Name"
           />
           <input
-            className={classes.login_form__input}
+            className={classes.signin_form__input}
             type="email"
             name="email"
             placeholder="Email-id"
           />
           <input
-            className={classes.login_form__input}
+            className={classes.signin_form__input}
             type="password"
             name="password"
             placeholder="Password"
           />
           <input
-            className={classes.login_form__input}
+            className={classes.signin_form__input}
             type="tel"
             name="phone"
             placeholder="Phone Number"
             pattern="[+]{46}[0-9]{11,14}"
             required
           />
-          <button className={classes.login_form__button} type="submit">
+          <button className={classes.signin_form__button} type="submit">
             SignIn
           </button>
-          <div className={classes.login_form__links}>
-            <a className={classes.login_form__link} href="./">
-              Forgot your password?
-            </a>
-          </div>
         </div>
       </form>
     </div>
--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -1,52 +1,53 @@
 import React from 'react';
 import classes from './SignUp.module.css';
 
-function SignIn() {
+function SignUp() {
   return (
     <div className={classes.form}>
-      <form className={classes.login_form} action="./" method="POST">
-        <div className={classes.login_form__content}>
-          <div className={classes.login_form__header}>
-            SignIn to your account
+      <form className={classes.signup_form} action="./" method="POST">
+        <div className={classes.signup_form__content}>
+          <div className={classes.signup_form__header}>
+            Signup to your account
           </div>
           <input
-            className={classes.login_form__input}
+            className={classes.signup_form__input}
             type="text"
             name="name"
             placeholder="Name"
           />
           <input
-            className={classes.login_form__input}
+            className={classes.signup_form__input}
             type="email"
             name="email"
             placeholder="Email-id"
           />
           <input
-            className={classes.login_form__input}
+            className={classes.signup_form__input}
             type="password"
             name="password"
             placeholder="Password"
           />
           <input
-            className={classes.login_form__input}
+            className={classes.signup_form__input}
+            type="password"
+            name="password_confirm"
+            placeholder="Confirm password"
+          />
+          <input
+            className={classes.signup_form__input}
             type="tel"
             name="phone"
             placeholder="Phone Number"
             pattern="[+]{46}[0-9]{11,14}"
             required
           />
-          <button className={classes.login_form__button} type="submit">
-            SignIn
+          <button className={classes.signup_form__button} type="submit">
+            SignUp
           </button>
-          <div className={classes.login_form__links}>
-            <a className={classes.login_form__link} href="./">
-              Forgot your password?
-            </a>
-          </div>
         </div>
       </form>
     </div>
   );
 }
 
-export default SignIn;
+export default SignUp;
