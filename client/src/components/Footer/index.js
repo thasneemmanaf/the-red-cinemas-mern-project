768f5fb087f988244e71095c1d7338e3038575d2
thasneemmanaf <123thasneemsalim@gmail.com>
2020-12-02 11:36:50 +0100

    Add Footer component using react-icons

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import {
+  FooterContainer,
+  FooterWrap,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcons,
+  SocialIconLink
+} from './style';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/">The RED Cinemas</SocialLogo>
+            <SocialIcons>
+              <SocialIconLink
+                href="//www.facebook.com"
+                target="_blank"
+                aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink
+                href="//www.instagram.com"
+                target="_blank"
+                aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink
+                href="//www.youtube.com"
+                target="_blank"
+                aria-label="Youtube">
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink
+                href="//www.twitter.com"
+                target="_blank"
+                aria-label="Twitter"
+                rel="noopener noreferrer">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink
+                href="//www.linkedin.com"
+                target="_blank"
+                aria-label="Linkedin">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
