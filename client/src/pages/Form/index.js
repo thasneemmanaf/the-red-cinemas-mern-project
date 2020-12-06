40c82d666341cb61be90e487311f74f49b50004b
thasneemmanaf <123thasneemsalim@gmail.com>
2020-12-06 17:39:23 +0100

    Remove login page image

--- client/src/pages/Form/index.js
+++ client/src/pages/Form/index.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import classes from './Form.module.css';
 import FormSignup from '../../components/SignUp';
 import FormSignin from '../../components/SignIn';
-import formImage from '../../images/formImg.jpg';
+// import formImage from '../../images/formImg.jpg';
 
 const Form = (props) => {
   const [isSubmitted, setIsSubmitted] = useState(false);
@@ -19,7 +19,11 @@ const Form = (props) => {
           <span className={classes.close_btn}>×</span>
         </NavLink>
         <div className={classes.form_content_left}>
-          <img className={classes.form_img} src={formImage} alt="spaceship" />
+          <img
+            className={classes.form_img}
+            src="https://images.pexels.com/photos/1769413/pexels-photo-1769413.jpeg?cs=srgb&dl=pexels-alex-powell-1769413.jpg&fm=jpg"
+            alt="spaceship"
+          />
         </div>
         {props.match.path === '/signup' ? (
           <FormSignup submitForm={submitForm} isSubmitted={isSubmitted} />
